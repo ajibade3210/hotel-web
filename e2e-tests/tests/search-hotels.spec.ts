ab04575662ecec175d06a560af5cf3dcfa3cd549
olaoluwa ajibade <ajibade69th@gmail.com>
2024-08-06 05:03:27 +0100

    add view hotel detail e2e test

--- e2e-tests/tests/search-hotels.spec.ts
+++ e2e-tests/tests/search-hotels.spec.ts
@@ -26,3 +26,14 @@ test("should show hotel search results", async ({ page }) => {
   await expect(page.getByText("Hotels found in Nigeria")).toBeVisible();
   await expect(page.getByText("Named Hotel")).toBeVisible();
 });
+
+test("should show hotel details", async ({ page }) => {
+  await page.goto(UI_URL);
+
+  await page.getByPlaceholder("Where are you going").fill("Nigeria");
+  await page.getByRole("button", { name: "Search" }).click();
+
+  await page.getByText("Named Hotel").click();
+  await expect(page).toHaveURL(/detail/);
+  await expect(page.getByRole("button", { name: "Book now" })).toBeVisible();
+});
